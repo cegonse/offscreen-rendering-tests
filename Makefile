d81e13259d3652784d4c287e6d7749f1166822d5
Cesar Gonzalez <cegonse@gmail.com>
2025-05-11 14:55:13 +0200

    Move rendering file to common

--- Makefile
+++ Makefile
@@ -4,12 +4,13 @@ testing-shaders:
 	@mkdir -p build
 	@g++ \
 		-Ilib \
+		-Icommon \
 		-I/usr/include/ImageMagick-6 \
 		-I/usr/include/x86_64-linux-gnu/ImageMagick-6 \
 		-Llib \
+		./common/render.cpp \
 		./testing-shaders/shader.test.cpp \
 		./testing-shaders/verify.cpp \
-		./testing-shaders/render.cpp \
 		./testing-shaders/image-compare.cpp \
 		-lraylib \
 		`pkg-config --cflags --libs MagickWand` \
--- testing-shaders/shader.test.cpp
+++ testing-shaders/shader.test.cpp
@@ -1,15 +1,15 @@
 #include <cest>
-#include "render.h"
+#include <render.h>
 #include "verify.h"
 
 describe("Post-processing Camera Shaders", []() {
   it("renders all pixels with bloom effect", []() {
-    RenderWithShader("testing-shaders/bloom.fs");
+    RenderWithShader("common/bloom.fs");
     Verify();
   });
 
   it("renders all pixels in B&W", []() {
-    RenderWithShader("testing-shaders/grayscale.fs");
+    RenderWithShader("common/grayscale.fs");
     Verify();
   });
 
