80dbc4c04dfd144d9861e0c66d71d34a3eb9b2b6
Cesar Gonzalez <cegonse@gmail.com>
2025-05-11 15:23:19 +0200

    Pass command line input to API renderer

--- http-api-rendering/main.cpp
+++ http-api-rendering/main.cpp
@@ -1,28 +1,16 @@
 #include <raylib.h>
 #include <render.h>
-#include <charconv>
 #include <cstring>
-
-float ParseFloat(char *start)
-{
-  char *end = std::strchr(start, '\0');
-  float result = 0.f;
-
-  const auto [_, success] = std::from_chars(start, end, result);
-  if (!(bool)success) return 0.f;
-
-  return result;
-}
 
 int main(int argc, char *argv[])
 {
   if (argc != 4) return 1;
 
-  auto x = ParseFloat(argv[1]);
-  auto y = ParseFloat(argv[2]);
-  auto z = ParseFloat(argv[3]);
+  auto x = std::stof(argv[1]);
+  auto y = std::stof(argv[2]);
+  auto z = std::stof(argv[3]);
 
-  RenderWithShader("common/bloom.fs");
+  RenderWithShader("common/bloom.fs", { x, y, z });
   TakeScreenshot("api-out.png");
 
   CleanUp();
