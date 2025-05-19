c239b53a65abf8b1d887708d1081d3759bc9ba12
Cesar Gonzalez <cegonse@gmail.com>
2025-05-19 18:03:49 +0200

    Force 360 frames in game test

--- integration-testing/game.test.cpp
+++ integration-testing/game.test.cpp
@@ -10,7 +10,7 @@ describe("JumpingGame", []() {
 
     onEveryNthFrame(6, [](int _) { Platform::ForceJumpKey(); });
     onEveryNthFrame(10, [](int frame) { Screenshot(frame); });
-    runFrames(200, [&]() { game.DoFrame(); });
+    runFrames(360, [&]() { game.DoFrame(); });
 
     expect(game.Score()).toBeGreaterThan(5);
   });
